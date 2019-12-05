84f0e77ee6884e2e474b1fb83faea40289b324aa
Gustavo <gustavostraubcovers@gmail.com>
2019-12-05 16:41:54 -0300

    getting all jokes types now

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,25 @@ import axios from 'axios'
 export default function index() {
 
   const [Piadas, setPiadas] = useState()
+  const [Piadas2, setPiadas2] = useState()
+  const [Piadas3, setPiadas3] = useState()
   useEffect(() => {
     axios.get('https://sv443.net/jokeapi/category/any')
-    .then(response => {setPiadas(response.data.id)}  )
+    .then(function (response) {
+      setPiadas(response.data.joke)
+      setPiadas2(response.data.setup)
+      setPiadas3(response.data.delivery)
+    })
 
   },[])
   console.log("aqui",Piadas)
   return (
     <div>
     {Piadas}
+    {Piadas2}
+    <div>
+    {Piadas3}
+    </div>
     </div>
   )
 }
